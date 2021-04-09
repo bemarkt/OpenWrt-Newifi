e947628cce9dcb27709fa380404ef5f9bb8e1539
bemarkt <bemarkthg@gmail.com>
2021-04-09 21:06:03 +0800

    update

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,12 +15,12 @@ rm -rf ./package/lean/luci-theme-argon
 # Add new packages
 # > Theme
 git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
-git clone https://github.com/Leo-Jo-My/luci-theme-opentomcat.git package/luci-theme-opentomcat
-# # > JD
+git clone https://github.com/Leo-Jo-My/luci-theme-opentomcat.git package/lean/luci-theme-opentomcat
+# > JD
 # git clone https://github.com/jerrykuku/luci-app-jd-dailybonus.git package/lean/luci-app-jd-dailybonus
 # > SmartDNS
-git clone https://github.com/pymumu/openwrt-smartdns package/smartdns
-git clone -b lede https://github.com/pymumu/luci-app-smartdns.git package/luci-app-smartdns
+git clone https://github.com/pymumu/openwrt-smartdns package/lean/smartdns
+git clone -b lede https://github.com/pymumu/luci-app-smartdns.git package/lean/luci-app-smartdns
 # mkdir -p package/lean/smartdns && wget -P package/lean/smartdns https://raw.githubusercontent.com/pymumu/openwrt-smartdns/master/Makefile
 # sed -i 's/PKG_VERSION:=./PKG_VERSION:=5.2020.04/g;s/PKG_SOURCE_VERSION:=./PKG_SOURCE_VERSION:=7af6f475da372842c48f570f44b52f48616b11d6/g' package/lean/smartdns/Makefile
 # git clone -b lede https://github.com/shaoyou11/luci-app-smartdns.git package/lean/luci-app-smartdns
